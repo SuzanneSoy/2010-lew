4ef36e37221e1c67d148f70385df568bd06e9cc0
Georges Dupéron <jahvascriptmaniac+github@free.fr>
2010-09-26 01:48:44 +0200

    Premier bloc affiché à l'écran.

--- main.c
+++ main.c
@@ -3,17 +3,22 @@
 #include <SDL/SDL.h>
 #include "erreurs.h"
 
+#define PROFONDEUR 24
+
 SDL_Surface* init() {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		die("Erreur lors de l'initialisation de SDL :");
 	}
 	
 	SDL_Surface* fenetre;
-	fenetre = SDL_SetVideoMode(640, 480, 24, SDL_HWSURFACE);
+	fenetre = SDL_SetVideoMode(640, 480, PROFONDEUR, SDL_HWSURFACE);
 	if (fenetre == NULL) {
 		die("Erreur lors de la création de la fenêtre.");
 	}
 	SDL_WM_SetCaption("Lew", "lew.png");
+	
+	SDL_FillRect(fenetre, NULL, SDL_MapRGB(fenetre->format, 0, 0, 0));
+	SDL_Flip(fenetre);
 	
 	return fenetre;
 }
@@ -26,10 +31,16 @@ void quit() {
 int main(int argc, char** argv) {
 	SDL_Surface* fenetre = init();
 	
-	SDL_FillRect(fenetre, NULL, SDL_MapRGB(fenetre->format, 255, 0, 128));
+	SDL_Surface* bloc = SDL_CreateRGBSurface(SDL_HWSURFACE, 11, 11, PROFONDEUR, 0, 0, 0, 0);
+	SDL_FillRect(bloc, NULL, SDL_MapRGB(fenetre->format, 255, 255, 255));
+	SDL_Rect position = {0,0};
+	
+	SDL_BlitSurface(bloc, NULL, fenetre, &position);
 	SDL_Flip(fenetre);
 	
 	while (1);
+	
+	SDL_FreeSurface(bloc);
 	
 	quit();
 }
